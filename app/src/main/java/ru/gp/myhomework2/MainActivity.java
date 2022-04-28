39beaa8cefd5472b328a3802f0f08bef609388b4
PookanBombers <tortellko@gmail.com>
2022-04-28 23:40:12 +0300

    добавил анимированный фон вместо картинки. я просто уже проходил это и пишу второй раз дабы все вспомнить. прописал стиль для кнопок.

--- app/src/main/java/ru/gp/myhomework2/MainActivity.java
+++ app/src/main/java/ru/gp/myhomework2/MainActivity.java
@@ -1,7 +1,9 @@
 package ru.gp.myhomework2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.mainColor);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setExitFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
     }
 }
